5841a5b89460bf1792fa1c9f33c110b60adc5ee2
Tova Yocheved Hillman <hillman.t.y@gmail.com>
2024-04-07 12:37:20 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Add your code here
+
+
+
+
+function submitData(userName, userEmail) {
+
+    
+
+    const formData = {
+        name: "",
+        email: "",
+    };
+    formData.name = userName;
+    formData.email = userEmail;
+
+    const postData = {
+        method: "POST",
+        headers: {
+            "content-Type": "application/json",
+            "accept": "application/json",
+        },
+        body: JSON.stringify(formData),
+    };
+    // object with name email 
+
+    return fetch("http://localhost:3000/users", postData)
+        .then(
+            function (resp) {
+                return resp.json()
+            })
+        .then(function (a) {
+            const userID = a.id;
+            const h = document.createElement("h1");
+            h.innerHTML = userID;
+            document.body.appendChild(h);
+
+        })
+.catch(function(errorMes) {
+    const message = errorMes.message;
+    const h = document.createElement("h1");
+    h.innerHTML = message;
+    document.body.appendChild(h);
+}
+
+);
+}
+
+
+
+
+        
